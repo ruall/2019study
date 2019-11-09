ffc4eaf1a2ba702a0c197a2199d0ef11dae13bf2
ling <linghuajie2019@gmail.com>
2019-11-09 11:14:52 +0800

    1

--- TS/参数个数不定的函数.ts
+++ TS/参数个数不定的函数.ts
@@ -5,19 +5,19 @@
  * */
 
 let fun_arr1 = (vals: number[]): number => {
-    let result:number = 0;
-    for (let val of vals) {
-        result += val
-    }
-    return result
+  let result: number = 0;
+  for (let val of vals) {
+    result += val
+  }
+  return result
 }
-console.log(fun_arr1([1,2,3])) // 6
+console.log(fun_arr1([1, 2, 3])) // 6
 
 let fun_arr2 = (...vals: number[]): number => {
-    let result:number = 0;
-    for (let val of vals) {
-        result += val
-    }
-    return result
+  let result: number = 0;
+  for (let val of vals) {
+    result += val
+  }
+  return result
 }
-console.log(fun_arr2(1,2,3)) // 6
+console.log(fun_arr2(1, 2, 3)) // 6
